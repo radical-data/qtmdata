25cc01dbe7da887a90ce17f0439a8e9cb1acc2c5
Jo Kroese <jo@jokroese.com>
2023-08-03 19:14:55 +0100

    be explicit in functions

--- data-raw/queeringthemap.R
+++ data-raw/queeringthemap.R
@@ -7,7 +7,7 @@ json <- httr2::resp_body_json(response)
 total_pages <- json$pages |> as.numeric()
 total_moments <- json$count |> as.numeric()
 
-queeringthemap <- tibble("v1" = numeric(), "v2" = numeric(), "v3" = character(), "v4" = numeric())
+queeringthemap <- tibble::tibble()
 
 for (i in 0:total_pages) {
   print(paste0("fetching page ", as.character(i), " (", 100*round(i/total_pages, 3), "%)"))
@@ -17,22 +17,22 @@ for (i in 0:total_pages) {
     httr2::req_perform()
   json <- httr2::resp_body_json(response)
   to_add <- json$moment_list |>
-    as_tibble(.name_repair = "minimal") |>
+    tibble::as_tibble(.name_repair = "minimal") |>
     t() |>
-    as_tibble() |>
-    mutate(across(everything(), unlist)) |>
+    tibble::as_tibble() |>
+    dplyr::mutate(across(everything(), unlist)) |>
     janitor::clean_names()
   queeringthemap <- queeringthemap |>
-    add_row(to_add)
+    dplyr::bind_rows(to_add)
   print(to_add)
 }
 
 queeringthemap <- queeringthemap |>
-  rename(latitude = v1,
+  dplyr::rename(latitude = v1,
          longitude = v2,
          content = v3,
          id = v4)
 
 print(paste0("recieved ", nrow(queeringthemap), " out of ", total_moments, " moments"))
 
-use_data(queeringthemap, overwrite = TRUE)
+usethis::use_data(queeringthemap, overwrite = TRUE)
